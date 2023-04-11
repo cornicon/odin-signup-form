145a7bb79b37f0801fcd43222a4ef61f7df4cef3
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-04-11 09:34:13 -0700

    Added Password Validation

--- javascript.js
+++ javascript.js
@@ -1,4 +1,16 @@
-function testFunction()
-{
-    console.log("The Javascript is linked");
+var password = document.getElementById("password")
+    , confirm_password = document.getElementById("password-confirm");
+
+function validatePassword() {
+    if(password.value != confirm_password.value)
+    {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    }
+    else
+    {
+        confirm_password.setCustomValidity("");
+    }
 }
+
+password.onchange = validatePassword;
+confirm.onkeyup = validatePassword;
