33d7b6acf4c424a71994c400efb518945f36b1e3
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-04-17 13:28:29 -0700

    Fixed Password Confirm Matching

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
 var password = document.getElementById("password")
-    , confirm_password = document.getElementById("password-confirm");
+    , confirm_password = document.getElementById("password-confirm")
+    , error_label = document.getElementById("password-error");
 
 function validatePassword() {
     if(password.value != confirm_password.value)
     {
         confirm_password.setCustomValidity("Passwords Don't Match");
+        error_label.innerHTML = "* Passwords Don't Match";
     }
     else
     {
         confirm_password.setCustomValidity("");
+        error_label.innerHTML = "";
     }
 }
 
 password.onchange = validatePassword;
-confirm.onkeyup = validatePassword;
+confirm_password.onchange = validatePassword;
